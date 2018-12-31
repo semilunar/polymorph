469977630384b7625455c854dd6e854dda57a7e1
katya n <31803873+semilunar@users.noreply.github.com>
2018-12-31 17:27:21 +0300

    Updates

---
+++ precache-manifest.c2c2320b5ea7b6b7d38fcfc6f7e87909.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07cd51215365aea07135",
+    "url": "/polymorph/static/js/runtime~main.07cd5121.js"
+  },
+  {
+    "revision": "df88dac61cc3cfd82bad",
+    "url": "/polymorph/static/js/main.df88dac6.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/polymorph/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "df88dac61cc3cfd82bad",
+    "url": "/polymorph/static/css/main.dc4ebe96.chunk.css"
+  },
+  {
+    "revision": "d2d8edd51127f49914a0b303a8671a37",
+    "url": "/polymorph/index.html"
+  }
+];
